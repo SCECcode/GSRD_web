8f170a9e80de2540117cfa2da15727aedf6cf80d
meihuisu <mei@ip-172-31-31-151.us-west-2.compute.internal>
2023-08-10 05:04:42 +0000

    wip

--- schema/scripts/extract-csv.sh
+++ schema/scripts/extract-csv.sh
@@ -11,7 +11,7 @@
 rm -f *.csv
 #in2csv --sheet "${EXCEL_NM_SHEET}" ${EXCEL_NM_FILE} | csvcut -c 1-26 > ${EXCEL_NM}_raw.csv
 
-cat ${EGDPATH}/${EXCEL_NM}.csv | csvcut -c 1-25 > ${EXCEL_NM}_raw.csv
+cat ${EGDPATH}/${EXCEL_NM}.csv | csvcut -c 1-28 > ${EXCEL_NM}_raw.csv
 cat ${EXCEL_NM}_raw.csv |sed "s/  / /g" | sed "s/, E/,E/"  > ${EXCEL_NM}.csv
 csvcut -n ${EXCEL_NM}.csv > ${EXCEL_NM}_column_labels
 
@@ -23,16 +23,16 @@ csvcut -c "9" ${EXCEL_NM}.csv |csvcut -K 1 | sort -n|uniq | sed "1i\\
 id,test
 "> id_test_tb.csv 
 
-# 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25
+# 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28
 #FaultName,FaultID,State,SiteName,
 #EDGId,SliprateId,
 #Longitude,Latitude,
 #DistToCFMFault,CFM6ObjectName,
 #DataType,
 #Observation,PrefRate,LowRate,HighRate,RateUncertainty,RateType,ReptReint,
-#OffsetType,AgeType,NumEvents,RateAge,QbinMin,QbinMax,Reference
-#Link,FullReference
+#OffsetType,AgeType,NumEvents,RateAge,QbinMin,QbinMax,
+#UCERF3 AppB,Short References,DOI/Web Link,Full References
 #
-csvcut -c "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27" ${EXCEL_NM}.csv |csvcut -K 1|sort -n|uniq | sed "1i\\
-FaultName,FaultID,State,SiteName,EGDId,SliprateId,Longitude,Latitude,DistToCFMFault,CFM6ObjectName,DataType,Observation,PrefRate,LowRate,HighRate,RateUnct,RateType,ReptReint,OffsetType,AgeType,NumEvents,RateAge,QbinMin,QbinMax,Reference,Link,FullReference
+csvcut -c "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28" ${EXCEL_NM}.csv |csvcut -K 1|sort -n|uniq | sed "1i\\
+FaultName,FaultID,State,SiteName,EGDId,SliprateId,Longitude,Latitude,DistToCFMFault,CFM6ObjectName,DataType,Observation,PrefRate,LowRate,HighRate,RateUnct,RateType,ReptReint,OffsetType,AgeType,NumEvents,RateAge,QbinMin,QbinMax,UCERF3AppB,ShortReferences,Links,FullReferences
 "> sliprate_site_tb.csv 
--- schema/scripts/sql_template/setup_sliprate_tb.sql
+++ schema/scripts/sql_template/setup_sliprate_tb.sql
@@ -1,1 +1,2 @@
-COPY SLIPRATE_tb(FaultName,FaultID,State,SiteName,EGDId,SliprateId,Longitude,Latitude,DistToCFMFault,CFM6ObjectName,DataType,Observation,PrefRate,LowRate,HighRate,RateUnct,RateType,ReptReint,OffsetType,AgeType,NumEvents,RateAge,QbinMin,QbinMax,Reference,Link,FullReference) FROM '/home/postgres/EGD/data/DATATYPE/sliprate_site_tb.csv' DELIMITER ',' CSV HEADER;
+COPY SLIPRATE_tb(FaultName,FaultID,State,SiteName,EGDId,SliprateId,Longitude,Latitude,DistToCFMFault,CFM6ObjectName,DataType,Observation,PrefRate,LowRate,HighRate,RateUnct,RateType,ReptReint,OffsetType,AgeType,NumEvents,RateAge,QbinMin,QbinMax,
+UCERF3AppB,ShortReferences,Links,FullReferences) FROM '/home/postgres/EGD/data/DATATYPE/sliprate_site_tb.csv' DELIMITER ',' CSV HEADER;
